578f783de266fe62eaa8a65380f947dba7c9263a
TurboBum <turbobum737@gmail.com>
2024-08-16 10:40:20 +0300

    Разбиение страницы

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    #Построчная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 1)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     return render(request, 'blog/post/list.html',{'posts':posts})
 
 def post_detail(request, year, month, day, post):
