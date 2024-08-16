02327fea1bf5e9d73aee32de50f174f0fb64849c
TurboBum <turbobum737@gmail.com>
2024-08-16 11:21:48 +0300

    Обработка ошибок постраничной разбивки

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_list(request):
     post_list = Post.published.all()
     #Построчная разбивка с 3 постами на страницу
-    paginator = Paginator(post_list, 1)
+    paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:#Если page_number не целое число, то выдать пераую страницу
+        posts = paginator.page(1)
+    except EmptyPage:#Если page_number находится вне диапазона, то выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html',{'posts':posts})
 
 def post_detail(request, year, month, day, post):
